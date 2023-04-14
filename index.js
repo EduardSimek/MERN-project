9715145d37c1d943c8d37d1aab7c05aa9292c15b
Eduard Šimek <89217170+EduardSimek@users.noreply.github.com>
2023-04-14 09:51:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log("Error connecting to MongoDB:", err.message);
+  }
+}
+
+connectToDB();
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log("Backend server is ready");
+});
